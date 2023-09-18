13fe697c0aacc7212bbc8bd0e4c7390162149704
mikkelJurs <s224279@student.dtu.dk>
2023-09-18 09:12:32 +0200

    Initial commit

---
+++ app/src/main/java/com/dtu/uemad/spellbook_35/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.dtu.uemad.spellbook_35.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/dtu/uemad/spellbook_35/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.dtu.uemad.spellbook_35.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
